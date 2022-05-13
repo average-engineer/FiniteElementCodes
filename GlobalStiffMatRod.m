ff8c4b333ec0eee9a1fb5338eee6430c0a136819
Ashutosh Mukherjee <ashutosh.mukherjeecpg@gmail.com>
2022-05-13 14:29:45 +0200

    Modified the code to solve Wx1 problem

--- GlobalStiffMatRod.m
+++ GlobalStiffMatRod.m
@@ -1,6 +1,6 @@
 %**********GLOBAL STIFFNESS MATRIX ASSEMBLY FOR 1D ROD ELEMENTS************
 
-% Last Modified: 29th October, 2021
+% Last Modified: 22nd April, 2022
 % Author: Ashutosh Mukherjee
 %**************************************************************************
 %% Settings
@@ -12,7 +12,7 @@ format short
 %% Problem Definition:
 % Number of Nodes
 n = 4;
-% Number of Elements: 5
+% Number of Elements: 6
 % Type of Elements: 1D Rod Elements (Linear Order Elements): 2 Nodes per
 % element
 % Number of DOFs per Nodes
@@ -25,19 +25,21 @@ A = 1; % Cross-Section (All elements have the same A)
 L = 1; % Element Length
 
 %% Creating the local element stiffness matrices (Problem Specific)
-K1 = RodElementStiffnessMatrix(0,E,A,L); % 1st Element
-K2 = RodElementStiffnessMatrix(pi/4,E,A,sqrt(2)*L); % 2nd Element
-K3 = RodElementStiffnessMatrix(pi/2,E,A,L); % 3rd Element
-K4 = RodElementStiffnessMatrix(0,E,A,L); % 4th Element
-K5 = RodElementStiffnessMatrix(3*pi/4,E,A,sqrt(2)*L); % 5th Element
+K1 = RodElementStiffnessMatrix(-pi/2,E,A,0.8*L); % 1st Element
+K2 = RodElementStiffnessMatrix(-2.214,E,A,L); % 2nd Element
+K3 = RodElementStiffnessMatrix(-pi,E,A,0.6*L); % 3rd Element
+K4 = RodElementStiffnessMatrix(-0.9273,E,A,L); % 4th Element
+K5 = RodElementStiffnessMatrix(-pi/2,E,A,0.8*L); % 5th Element
+K6 = RodElementStiffnessMatrix(pi,E,A,0.6*L); % 6th Element
 
 %% Storing the element stiffness matrices in one array
-nelem = 5; % Number of Elements
+nelem = 6; % Number of Elements
 elemStiff{1} = K1;
 elemStiff{2} = K2;
 elemStiff{3} = K3;
 elemStiff{4} = K4;
 elemStiff{5} = K5;
+elemStiff{6} = K6;
 
 %% Associated Nodes with each element
 % Assumption: The angles the elements make is considered from lower global
@@ -49,11 +51,12 @@ elemStiff{5} = K5;
 % 2 Nodes for each element
 assNodes = NaN(nelem,2);
 
-assNodes(1,:) = [1,2]; % Nodes associated with element 1
-assNodes(2,:) = [1,3]; % Nodes associated with element 2
-assNodes(3,:) = [2,3]; % Nodes associated with element 3
-assNodes(4,:) = [2,4]; % Nodes associated with element 4
-assNodes(5,:) = [3,4]; % Nodes associated with element 5
+assNodes(1,:) = [1,3]; % Nodes associated with element 1
+assNodes(2,:) = [1,4]; % Nodes associated with element 2
+assNodes(3,:) = [1,2]; % Nodes associated with element 3
+assNodes(4,:) = [2,3]; % Nodes associated with element 4
+assNodes(5,:) = [2,4]; % Nodes associated with element 5
+assNodes(6,:) = [3,4]; % Nodes associated with element 6
 
 %% Assembling the Global Stiffness Matrix
 KGlobal = zeros(n*ndof,n*ndof); % Assembled Global Stiffness Matrix
